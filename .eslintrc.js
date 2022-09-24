5deda9d8ca42ee22e3fac7aee58de8a313ad9d84
Muhammad Huzaifa <muhammadhuzaifa@Tintashs-MacBook-Pro.local>
2022-09-24 23:10:35 +0500

    initial commit: setup husky with eslint, prettier, and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// Path: /.eslintrc.js
+
+module.exports = {
+  extends: ['react-app', 'airbnb'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'function-paren-newline': 'off',
+    // You can override any rules you want
+  },
+};
