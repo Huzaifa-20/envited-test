c16bf4ad56d6183c98527d147bce0ccd9e5d33ff
Muhammad Huzaifa <muhammadhuzaifa@Tintashs-MacBook-Pro.local>
2022-09-25 02:56:09 +0500

    feat: create event page

--- src/pages/event_page/EventPage.js
+++ src/pages/event_page/EventPage.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import './EventPage.css';
+
 const EventPage = () => {
-  return <h1>Event Page</h1>;
+  return (
+    <div
+      className="page-container"
+      style={{ justifyContent: 'start', padding: '0' }}
+    >
+      <div className="image-container" />
+      <div className="heading-container">
+        <h1 className="page-heading" style={{ textAlign: 'start' }}>
+          Birthday Bash
+        </h1>
+        <p className="page-text">
+          Easily host and share events with your friends across any social
+          media.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default EventPage;
